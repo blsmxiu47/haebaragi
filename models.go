58c097bb323f62959b1a18d54b9b79a9f185f1f5
Wes Warriner <fridgehistory@gmail.com>
2023-12-23 10:22:30 -0700

    add json names to Word type

--- models.go
+++ models.go
@@ -1,14 +1,14 @@
 package main
 
 type Word struct {
-	ID                  int
-	SourceText          string
-	TranslationText     string
-	SourceLanguage      string
-	TranslationLanguage string
-	PartOfSpeech        string
-	Gender              string
-	Category            string
-	Examples            []string
-	Notes               string
+	ID                  int     
+	SourceText          string   `json:"id"`
+	TranslationText     string   `json:"translationText"`
+	SourceLanguage      string   `json:"sourceLanguage"`
+	TranslationLanguage string   `json:"translationLanguage"`
+	PartOfSpeech        string   `json:"partOfSpeech"`
+	Gender              string   `json:"gender"`
+	Category            string   `json:"category"`
+	Examples            []string `json:"examples"`
+	Notes               string   `json:"notes"`
 }
