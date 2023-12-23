a0c7f67a01898afd1351281b6c1b661a3a319f40
Wes Warriner <fridgehistory@gmail.com>
2023-12-23 10:23:24 -0700

    update testWords with another entity; refactor main func and include handling of /word endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
-package main;
+package main
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -12,20 +13,37 @@ var testWords = []Word {
 		SourceLanguage: "English",
 		TranslationLanguage: "Deutsch",
 		Category: "",
-		PartOfSpeech: "",
+		PartOfSpeech: "noun",
 		Gender: "f",
 		Examples: []string{"Die Sonnenblume ist groß."},
 		Notes: "",
 	},
-}
-
-func main() {
-	http.HandleFunc("/", homeHandler)
-	http.ListenAndServe(":8080", nil)
-
-	// http.HandleFunc("/word", addWordHandler)
+	{
+		ID: 2,
+		SourceText: "German",
+		TranslationText: "Deutsch",
+		SourceLanguage: "English",
+		TranslationLanguage: "Deutsch",
+		Category: "",
+		PartOfSpeech: "noun",
+		Gender: "n",
+		Examples: []string{"Ich spreche kein Deutsch."},
+		Notes: "",
+	},
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("해바라기에 환영합니다!"))
+}
+
+func handleRequests() {
+    	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/word", addWordHandler)
+    	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	http.HandleFunc("/word", addWordHandler)
+}
+
+func main() {
+	handleRequests()
 }
