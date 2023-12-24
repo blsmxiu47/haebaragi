0219ef69061201511b7255df51e8529cde749b7b
Wes Warriner <fridgehistory@gmail.com>
2023-12-24 09:17:19 -0700

    add handlers for additional endpoints

--- handlers.go
+++ handlers.go
@@ -6,5 +6,25 @@ import (
 )
 
 func addWordHandler(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(testWords);
+	json.NewEncoder(w).Encode(testWords)
+}
+
+func addExpressionHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(testExpressions)
+}
+
+func addLanguageHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(testLanguages)
+}
+
+func addPartOfSpeechHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(testPartsOfSpeech)
+}
+
+func addGenderHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(testGenders)
+}
+
+func addCategoryHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(testCategories)
 }
