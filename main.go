c104eee09596cd573d98c5829b926e66f81c2c3c
Wes Warriner <fridgehistory@gmail.com>
2023-12-24 09:18:09 -0700

    add test expressions, languages, etc, and handling for these new endpoints

--- main.go
+++ main.go
@@ -32,6 +32,95 @@ var testWords = []Word{
 	},
 }
 
+var testExpressions = []Expression{
+	{
+		ID:                  1,
+		SourceText:          "to be on the ball",
+		TranslationText:     "auf Draht sein",
+		SourceLanguage:      "English",
+		TranslationLanguage: "Deutsch",
+		Category:            "",
+		Notes:               "",
+	},
+}
+
+var testLanguages = []Language{
+	{
+		ID:           1,
+		Name:         "English",
+		Abbreviation: "EN",
+	},
+	{
+		ID:           2,
+		Name:         "Deutsch",
+		Abbreviation: "DE",
+	},
+}
+
+var testCategories = []Category{
+	{
+		ID:   1,
+		Name: "Greetings",
+	},
+	{
+		ID:   2,
+		Name: "Food",
+	},
+	{
+		ID:   3,
+		Name: "People",
+	},
+}
+
+var testGenders = []Gender{
+	{
+		ID:           1,
+		Name:         "neuter",
+		Abbreviation: "n",
+		Languages:    []Language{testLanguages[1]},
+	},
+	{
+		ID:           2,
+		Name:         "masculine",
+		Abbreviation: "m",
+		Languages:    []Language{testLanguages[1]},
+	},
+	{
+		ID:           3,
+		Name:         "feminine",
+		Abbreviation: "f",
+		Languages:    []Language{testLanguages[1]},
+	},
+}
+
+var testPartsOfSpeech = []PartOfSpeech{
+	{
+		ID:           1,
+		Name:         "noun",
+		Abbreviation: "n",
+	},
+	{
+		ID:           2,
+		Name:         "verb",
+		Abbreviation: "v",
+	},
+	{
+		ID:           3,
+		Name:         "adjective",
+		Abbreviation: "adj",
+	},
+	{
+		ID:           4,
+		Name:         "adverb",
+		Abbreviation: "adv",
+	},
+	{
+		ID:           5,
+		Name:         "preposition",
+		Abbreviation: "prep",
+	}, // TODO: will add all when we have actual DB set up
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("해바라기에 환영합니다!"))
 }
@@ -39,6 +128,11 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 func handleRequests() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/word", addWordHandler)
+	http.HandleFunc("/expression", addExpressionHandler)
+	http.HandleFunc("/language", addLanguageHandler)
+	http.HandleFunc("/category", addCategoryHandler)
+	http.HandleFunc("/part-of-speech", addPartOfSpeechHandler)
+	http.HandleFunc("/gender", addGenderHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
