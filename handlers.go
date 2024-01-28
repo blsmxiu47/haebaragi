fdd39527621d4773e6ef5cb05c4505d4286c92b1
Wes Warriner <fridgehistory@gmail.com>
2024-01-28 09:17:46 -0700

    add more specific and targeted validation for word POST requests

--- handlers.go
+++ handlers.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"strings"
 )
+
+// Constants
+const MAX_REQUEST_BODY_SIZE_MSG = "1MB"
+const MAX_REQUEST_BODY_SIZE_BYTES = 1048576
 
 // Testing
 var words []Word
@@ -33,10 +42,56 @@ func wordHandler(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		json.NewEncoder(w).Encode(testWords)
 	case "POST":
+		ct := r.Header.Get("content-type")
+		if ct != "application/json" {
+			msg := "Invalid content-type. Content-Type is not application/json"
+			http.Error(w, msg, http.StatusUnsupportedMediaType)
+			return
+		}
+
+		r.Body = http.MaxBytesReader(w, r.Body, MAX_REQUEST_BODY_SIZE_BYTES)
+
+		dec := json.NewDecoder(r.Body)
+		dec.DisallowUnknownFields()
+
 		var word Word
-		err := json.NewDecoder(r.Body).Decode(&word)
+		err := dec.Decode(&word)
+		print(r.Body)
 		if err != nil {
-			http.Error(w, "Error reading word from request", http.StatusBadRequest)
+			var syntaxError *json.SyntaxError
+			var unmarshalTypeError *json.UnmarshalTypeError
+
+			switch {
+			case errors.As(err, &syntaxError):
+				msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
+				http.Error(w, msg, http.StatusBadRequest)
+
+			case errors.Is(err, io.ErrUnexpectedEOF):
+				msg := "Request body contains badly-formed JSON"
+				http.Error(w, msg, http.StatusBadRequest)
+
+			case errors.As(err, &unmarshalTypeError):
+				msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
+				http.Error(w, msg, http.StatusBadRequest)
+
+			case strings.HasPrefix(err.Error(), "json: unknown field "):
+				fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+				msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
+				http.Error(w, msg, http.StatusBadRequest)
+
+			case errors.Is(err, io.EOF):
+				msg := "Request body must not be empty"
+				http.Error(w, msg, http.StatusBadRequest)
+
+			case err.Error() == "http: request body too large":
+				msg := fmt.Sprintf("Request body must not be larger than %s", MAX_REQUEST_BODY_SIZE_MSG)
+				http.Error(w, msg, http.StatusRequestEntityTooLarge)
+
+			default:
+				log.Print(err.Error())
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+
 			return
 		}
 
