d8fbd2607dedf51ddc1072334897e00f84e6b7f3
Wes Warriner <fridgehistory@gmail.com>
2024-01-29 09:44:32 -0700

    replicate word POST request error handling for other routes

--- handlers.go
+++ handlers.go
@@ -29,6 +29,39 @@ func addLanguageToStore(language Language) error {
 	return nil
 }
 
+// Testing
+var partsOfSpeech []PartOfSpeech
+var nextPartOfSpeechID int = 1
+
+func addPartOfSpeechToStore(partOfSpeech PartOfSpeech) error {
+	partOfSpeech.ID = nextPartOfSpeechID
+	nextPartOfSpeechID++
+	partsOfSpeech = append(partsOfSpeech, partOfSpeech)
+	return nil
+}
+
+// Testing
+var genders []Gender
+var nextGenderID int = 1
+
+func addGenderToStore(gender Gender) error {
+	gender.ID = nextGenderID
+	nextGenderID++
+	genders = append(genders, gender)
+	return nil
+}
+
+// Testing
+var categories []Category
+var nextCategoryID int = 1
+
+func addCategoryToStore(category Category) error {
+	category.ID = nextCategoryID
+	nextCategoryID++
+	categories = append(categories, category)
+	return nil
+}
+
 // TODO: obvi these handlers will all do basically the same thing but for different data, so refactor
 func wordHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
@@ -75,9 +108,16 @@ func languageHandler(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(testLanguages)
 	case "POST":
 		var language Language
-		err := json.NewDecoder(r.Body).Decode(&language)
+
+		err := decodeJSONBody(w, r, &language)
 		if err != nil {
-			http.Error(w, "Error reading language from request", http.StatusBadRequest)
+			var mr *malformedRequest
+			if errors.As(err, &mr) {
+				http.Error(w, mr.msg, mr.status)
+			} else {
+				log.Println(err.Error())
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
 			return
 		}
 
@@ -97,13 +137,105 @@ func languageHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func partOfSpeechHandler(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(testPartsOfSpeech)
+	switch r.Method {
+	case "GET":
+		json.NewEncoder(w).Encode(testPartsOfSpeech)
+	case "POST":
+		var partOfSpeech PartOfSpeech
+
+		err := decodeJSONBody(w, r, &partOfSpeech)
+		if err != nil {
+			var mr *malformedRequest
+			if errors.As(err, &mr) {
+				http.Error(w, mr.msg, mr.status)
+			} else {
+				log.Println(err.Error())
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+			return
+		}
+
+		// TODO: Add validation for partOfSpeech fields
+
+		err = addPartOfSpeechToStore(partOfSpeech)
+		if err != nil {
+			http.Error(w, "Error saving partOfSpeech", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(partOfSpeech)
+	default:
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
 }
 
 func genderHandler(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(testGenders)
+	switch r.Method {
+	case "GET":
+		json.NewEncoder(w).Encode(testGenders)
+	case "POST":
+		var gender Gender
+
+		err := decodeJSONBody(w, r, &gender)
+		if err != nil {
+			var mr *malformedRequest
+			if errors.As(err, &mr) {
+				http.Error(w, mr.msg, mr.status)
+			} else {
+				log.Println(err.Error())
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+			return
+		}
+
+		// TODO: Add validation for gender fields
+
+		err = addGenderToStore(gender)
+		if err != nil {
+			http.Error(w, "Error saving gender", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(gender)
+	default:
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+
 }
 
 func categoryHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		json.NewEncoder(w).Encode(testCategories)
+	case "POST":
+		var category Category
+
+		err := decodeJSONBody(w, r, &category)
+		if err != nil {
+			var mr *malformedRequest
+			if errors.As(err, &mr) {
+				http.Error(w, mr.msg, mr.status)
+			} else {
+				log.Println(err.Error())
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+			return
+		}
+
+		// TODO: Add validation for category fields
+
+		err = addCategoryToStore(category)
+		if err != nil {
+			http.Error(w, "Error saving category", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(category)
+	default:
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
 	json.NewEncoder(w).Encode(testCategories)
 }
